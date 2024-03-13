5071fca8485f64af5cf716624bef11ff2cbd8a34
Adilkhan <aomertayevich@gmail.com>
2024-03-13 23:35:02 +0600

    added service logic file

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import json
 import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-logs_dir = r'/var/ossec/logs/alerts'
-host = '0.0.0.0'
-port = 8000
-origins = [
-'http://192.168.175.1'
-]
+from service import get_logs_from_wazuh
+from service import get_log_from_wazuh
+
+host = os.environ.get("HOST")
+port = os.environ.get("PORT")
+origins = [os.environ.get("ORIGIN")]
 
 app = FastAPI()
 
@@ -22,29 +21,17 @@ app.add_middleware(
 )
 
 
-def get_logs_from_wazuh():
-    os.chdir(logs_dir)
-    with open('alerts.json', 'r') as logs:
-        return logs.read()
-
-
-def get_log_from_wazuh(log_id: float):
-    os.chdir(logs_dir)
-    with open('alerts.json', 'r') as logs:
-        output = json.load(logs)
-        return output
-
-
-@app.get('/logs')
+@app.get("/logs")
 def get_logs():
     logs = get_logs_from_wazuh()
     return logs
 
-@app.get('/log')
+
+@app.get("/log")
 def get_log():
-    log = get_logs_from_wazuh()
+    log = get_log_from_wazuh()
     return log
 
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host=host, port=port)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host=host, port=port)
