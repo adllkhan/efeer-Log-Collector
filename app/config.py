05a18f6d4090d9f5099cc204839c8eace26a0ca3
Adilkhan <aomertayevich@gmail.com>
2024-03-15 19:41:30 +0600

    adjusted pattern

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
+# Base Config
 host = "0.0.0.0"
 port = 8000
 origins = ["0.0.0.0"]
-alerts_dir = "/var/ossec/logs/alerts"
+
+
+# Wazuh Config
+class Wazuh:
+    alerts_dir = "/var/ossec/logs/alerts"
---
+++ app/src/v1/endpoints/__init__.py
@@ -0,0 +1,1 @@
+from wazuh import router as wazuh_router
